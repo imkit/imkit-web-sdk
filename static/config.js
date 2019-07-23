d66311b3f45a72db6b6737ecf0717874e646c774
gagu-homePC <gagu@insowe.com>
2019-07-23 18:12:49 +0800

    樣式調整 icon 調整 i18n 修正 demo 頁面修改 點選聊天室內的其他人的頭像時，會跳出 alert 顯示放大頭像 收回訊息 訊息加密(未完成) 付款功能 修改子選單顯示的位置 config 新增下載音檔開關 config 功能按鈕新增 mobileEnable 如果聊天室內只有 2 人，則不顯示(人數) 修改網址解析規則 圖片檢視器動態左右箭頭 用 localForage + base64 存圖片 請款付款功能 右側資訊欄增加滑動效果 推播訊息 i18n 檔案拖曳感應範圍 bug 至頂房間加圖釘 地圖預設位置調整 圖片縮放bug 新聊天室已讀BUG 地圖載入前及傳送成功前 disable 傳送按鈕

--- static/config.js
+++ static/config.js
@@ -4,15 +4,17 @@ var config = {
   // chat server 位置
   domain: 'https://chat.fangho.com',
   // auth server 位置，與 authClientId 配合取得 token，若填 token，此值可不填
-  authBase: 'https://auth.imkit.io',
+  authBase: 'https://auth.fangho.com',
   // 登入的 chat user id，與 authBase 配合取得 token，若填 token，此值可不填
   authClientId: '',
   // 取得 url 預覽內容的 api 網址
-  urlPreviewApi: 'https://chatkit.co/url',
+  urlPreviewApi: 'https://url.imkit.io/',
   // chat server clientkey
   clientKey: 'fangho_imkit_0412_2018_001_clientkey',
   // chat user token，選填，若不填則需填 authBase 與 authClientId
   token: '',
+  // 訊息解密金鑰
+  privateKey: '',
   // S3 bucketName
   bucketName: 'chatserver-upload',
   // google api key，需要啟用 Maps JavaScript API, Maps Static API, Geocoding API
@@ -58,12 +60,15 @@ var config = {
   },
   // 中間聊天區塊設定
   chat: {
+    // 是否顯示聊天區塊的 header
+    showHeader: true,
     // 下方聊天工具按鈕設定
     actions: {
       // 表情符號
       emoji: {
         // 是否啟用
         enable: true,
+        mobileEnable: true,
         // 描述文字
         text: 'Send Emoji'
       },
@@ -71,6 +76,7 @@ var config = {
       sticker: {
         // 是否啟用
         enable: true,
+        mobileEnable: true,
         // 描述文字
         text: 'Send Sticker'
       },
@@ -78,11 +84,17 @@ var config = {
       image: {
         // 是否啟用
         enable: true,
+        mobileEnable: true,
         // 描述文字
         text: 'Send Image',
         extra: {
           // 限制檔案格式
-          accept: ['image/png', 'image/jpeg'],
+          accept: [
+            'image/png',
+            'image/jpeg',
+            'image/heic',
+            'image/heic-sequence'
+          ],
           // 限制檔案大小 (MB)
           limitSize: 10,
           // 縮圖的最大寬高 (px)
@@ -93,6 +105,7 @@ var config = {
       video: {
         // 是否啟用
         enable: true,
+        mobileEnable: true,
         // 描述文字
         text: 'Send Video',
         extra: {
@@ -108,6 +121,7 @@ var config = {
       file: {
         // 是否啟用
         enable: true,
+        mobileEnable: true,
         // 描述文字
         text: 'Send File',
         extra: {
@@ -121,6 +135,7 @@ var config = {
       recorder: {
         // 是否啟用
         enable: true,
+        mobileEnable: true,
         // 描述文字
         text: 'Send Recorder',
         extra: {
@@ -132,8 +147,38 @@ var config = {
       location: {
         // 是否啟用
         enable: true,
+        mobileEnable: true,
         // 描述文字
         text: 'Send Location'
+      },
+      // 金流
+      paymentRequest: {
+        // 是否啟用
+        enable: false,
+        mobileEnable: false,
+        // 描述文字
+        text: 'Send Payment Request',
+        extra: {
+          // 支援的幣值
+          currencies: ['TWD', 'USD'],
+          // 請款 API
+          requestApi: '',
+          paymentBy: [
+            {
+              key: 'paypal',
+              text: 'PayPal'
+            },
+            {
+              key: 'tappay',
+              text: 'TapPay'
+            },
+            {
+              currencies: ['TWD'],
+              key: 'newebpay',
+              text: '藍新'
+            }
+          ]
+        }
       }
     },
     // 聊天按鈕顯示位置 'bottom' or 'right'
@@ -146,8 +191,15 @@ var config = {
     sliderReturnMode: 'close',
     // 爬 url meta 時，是否使用異步讀取
     asyncUrlPreview: true,
-    // 自訂訊息框顏色，顏色格式：'#123456' 或 'rgba(12, 34, 56, 0.5)'
+    // 自訂顏色，顏色格式：'#123456' 或 'rgba(12, 34, 56, 0.5)'
     colors: {
+      // Header 顏色設定
+      header: {
+        // 背景色
+        background: null,
+        // 文字顏色
+        color: null
+      },
       // 自己發送的訊息
       self: {
         // 背景色
@@ -177,10 +229,32 @@ var config = {
     // 是否開啟回覆功能
     reply: true,
     // 是否開啟轉傳功能
-    forward: true
+    forward: true,
+    // 是否開啟收回訊息功能
+    recall: true,
+    // 是否可以下載音檔
+    audioDownload: true
   },
   // 左側聊天列表設定
   list: {
+    // 排序方式，有效值 null, 'createdTime'
+    sort: null,
+    // 可開啟的房間數量，設 0 不限制
+    maxCount: 0,
+    // 鎖定的房間顯示文字
+    lockText: '',
+    // 點即鎖定的房間觸發事件
+    lockOnClick: function() {},
+    // 自訂顏色，顏色格式：'#123456' 或 'rgba(12, 34, 56, 0.5)'
+    colors: {
+      // Header 顏色設定
+      header: {
+        // 背景色
+        background: null,
+        // 文字顏色
+        color: null
+      }
+    },
     // 是否顯示成員數
     memberCount: true,
     logout: {
@@ -199,6 +273,16 @@ var config = {
   },
   // 右側聊天資訊欄設定
   info: {
+    // 自訂顏色，顏色格式：'#123456' 或 'rgba(12, 34, 56, 0.5)'
+    colors: {
+      // Header 顏色設定
+      header: {
+        // 背景色
+        background: null,
+        // 文字顏色
+        color: null
+      }
+    },
     // 是否可編輯
     edit: {
       // 是否可編輯房間
@@ -246,7 +330,7 @@ var config = {
       if (error.RC === 401) {
         window.location.href = '../demo';
       } else {
-        alert(error.RM);
+        console.error(error.RM);
       }
     }
   },
@@ -279,7 +363,8 @@ var config = {
       'Send Video': 'Send Video',
       'Send File': 'Send File',
       'Send Recorder': 'Send Recorder',
-      'Send Location': 'Send Location'
+      'Send Location': 'Send Location',
+      'Send Payment Request': 'Send Payment Request'
     },
     'zh-tw': {
       Test: '測試',
@@ -289,7 +374,8 @@ var config = {
       'Send Video': '傳送影片',
       'Send File': '傳送檔案',
       'Send Recorder': '傳送錄音',
-      'Send Location': '傳送位置訊息'
+      'Send Location': '傳送位置訊息',
+      'Send Payment Request': '傳送請款訊息'
     }
   },
   // 客製功能
